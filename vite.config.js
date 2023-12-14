a962fb202326d2d055b42300e65d7a26fb026bdc
M. Shokhibul Anwar <73224918+ShoAnn@users.noreply.github.com>
2023-12-14 08:33:43 +0700

    base: /company-profile-website

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/company-profile-website',
   plugins: [react()],
   server: {
     port: 3000,
